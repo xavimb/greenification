70a67e67aae89c942698f4d13a17e028a6b4fc7e
sergioisidoro <sergio.isidoro@ist.utl.pt>
2014-05-24 21:44:19 +0300

    ErAdding Push notifications Schema

--- models.js
+++ models.js
@@ -10,25 +10,42 @@ db.once('open', function callback () {
   console.log("Database connected, yay!");
 });
 
+// DOMAIN SCHEMA
+
 var greenSchema = mongoose.Schema({
     buildingID: Number,
     address : String,
     city : String,
     points : Number,
     sensors : [{sensorID: Number, 
-              measurments: [{timeStamp : Date, 
-                          energy: Number, 
-                          device: String
-                          }]
+                device: String,
+                measurments: [{timeStamp : Date, 
+                              energy: Number 
+                            }]
             }],
     user: String // Only supports one user for now
 })
 
-var Green = mongoose.model('Green', greenSchema)
-
-greenSchema.methods.getUserBuildings = function(username) {
+greenSchema.methods.getUserBuilding = function(username) {
   return this.model('Green').find({ user: username });
 }
+
+greenSchema.methods.getBuildingSensors = function(building_id) {
+  return this.model('Green').find({ buildingID: building_id }.sensors);
+}
+
+//var Green = mongoose.model('Green', greenSchema)
+
+// NOTIFICATION SCHEMA
+
+var pushSchema = mongoose.Schema({
+    timeStamp: Date,
+    username: String,
+    notificationID: Number
+})
+
+
+
 
 
 
