17e9d2ba800f936d993bcb5b43abe52c77b667d0
Xavi Magrinyà <xavi.mb89@gmail.com>
2014-05-24 21:46:26 +0300

    AI improved. Checks the sunrise and sunset

--- ai.js
+++ ai.js
@@ -1,3 +1,17 @@
-exports.getTips = function (current_weather, weather_forecast, response) {
-	response.send(200);
+var TipsEnum = {
+	LIGHTS_OFF : {'id': 1, 'name': 'Good morning! You can turn off the lights.'}
+};
+
+exports.getTips = function (current_weather, weather_forecast, res) {
+	var currentDate = new Date();
+	var sunriseDate = new Date(current_weather.sys.sunrise);
+	var sunsetDate = new Date(current_weather.sys.sunset);
+	var tips = [];
+
+	// TODO: check first in the database
+	if(currentDate > sunrise && currentDate < sunset) {
+		tips.push(TipsEnum.LIGHTS_OFF);
+	}
+	
+	res.send(200);
 }
