908d13861dd116ab455f2b014ba5944dd3331cd0
sergioisidoro <sergio.isidoro@ist.utl.pt>
2014-05-24 20:24:40 +0300

    First Model, not tested yet

--- models.js
+++ models.js
@@ -7,6 +7,35 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', function callback () {
-	console.log("Database connected, yay!");
+  console.log("Database connected, yay!");
 });
+
+var greenSchema = mongoose.Schema({
+    buildingID: ObjectId,
+    address : String,
+    city : String,
+    points : Number
+    sensors : [sensorID: Number, 
+              measurments: [timeStamp : Date, 
+                          energy: Number, 
+                          device: String
+                          ]
+            ]
+    user: String // Only supports one user for now
+})
+
+var Green = mongoose.model('Green', greenSchema)
+
+greenSchema.methods.getUserBuildings = function(username) {
+  return this.model('Green').find({ user: username });
+}
+
+
+
+
+
+
+
+
+
 
